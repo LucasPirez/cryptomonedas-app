0d145ddac76bb0129b22453a59284bac8fcf528a
Lucas Pirez <lucaspirez42@gmail.com>
2023-02-04 10:49:45 -0300

    update tipeSubscriptions

--- components/user/subscriptions/TipeSubscriptions.js
+++ components/user/subscriptions/TipeSubscriptions.js
@@ -1,3 +1,5 @@
+import { color } from "../../../styles/colors";
+
 export default function TipeSubscriptions() {
   return (
     <>
@@ -11,7 +13,6 @@ export default function TipeSubscriptions() {
           <span className="price">$0.00/lifeTime</span>
 
           <button>Current Plan</button>
-          <br />
 
           <h4>Top Features</h4>
 
@@ -41,7 +42,6 @@ export default function TipeSubscriptions() {
           <span className="price">$41.66 /mo</span>
 
           <button>Upgrade to Premium</button>
-          <br />
 
           <h4>Everything in Free, plus:</h4>
 
@@ -72,10 +72,63 @@ export default function TipeSubscriptions() {
           height: 100vh;
           display: flex;
           justify-content: center;
+          align-items: flex-start;
           gap: 40px;
+          flex-wrap: wrap;
+          padding: 3rem;
         }
+        h3 {
+          font-size: 2rem;
+          line-height: 0.4rem;
+        }
+
+        span {
+          font-size: 2.7rem;
+          font-weight: bold;
+          margin-top: 2rem;
+        }
+
+        button {
+          padding: 1.4rem;
+          width: 80%;
+          border: none;
+          background: ${color.candleGreen};
+          color: #fff;
+          font-size: 1.2rem;
+          margin: 1.5rem 0 2rem 0;
+          opacity: 0.8;
+          transition: all 0.3s;
+          cursor: pointer;
+          border-radius: 5px;
+          box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px,
+            rgba(0, 0, 0, 0.24) 0px 1px 2px;
+        }
+        button:hover {
+          opacity: 1;
+          box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 2px,
+            rgba(0, 0, 0, 0.24) 0px 2px 2px;
+        }
+
+        h4 {
+          font-size: 1.6rem;
+        }
+
+        li {
+          margin-bottom: 2rem;
+        }
+
         .container {
-          width: minmax(350px, 550px);
+          max-width: 450px;
+          min-width: 380px;
+          height: 840px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          border-radius: 5px;
+          box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px,
+            rgba(0, 0, 0, 0.24) 0px 1px 2px;
+          border-top: 30px solid #4a4a4a;
+          padding: 0 2.4rem;
         }
       `}</style>
     </>
