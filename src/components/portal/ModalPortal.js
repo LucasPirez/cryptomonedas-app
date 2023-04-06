ab75cf0dfb9546817fa710353d3e4be77d2e12e5
Lucas Pirez <lucaspirez42@gmail.com>
2023-04-06 10:06:14 -0300

    deleteCypress

--- src/components/portal/ModalPortal.js
+++ src/components/portal/ModalPortal.js
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
-import ReactDom from "react-dom";
+import { useEffect } from 'react'
+import ReactDom from 'react-dom'
 
 export default function ModalPortal({ children }) {
-  const portalNode = document.createElement("div");
+  const portalNode = document.createElement('div')
 
-  portalNode.style.position = "fixed";
-  portalNode.style.width = "100vw";
-  portalNode.style.height = "100vh";
-  portalNode.style.display = "flex";
-  portalNode.style.justifyContent = "center";
-  portalNode.style.background = "#fafafa";
-  portalNode.style.top = "15vh";
-  portalNode.style.left = "0";
+  portalNode.style.position = 'fixed'
+  portalNode.style.width = '100vw'
+  portalNode.style.height = '100vh'
+  portalNode.style.display = 'flex'
+  portalNode.style.justifyContent = 'center'
+  portalNode.style.background = '#fafafa'
+  portalNode.style.top = '15vh'
+  portalNode.style.left = '0'
 
   useEffect(() => {
-    document.body.appendChild(portalNode);
+    document.body.appendChild(portalNode)
 
     return () => {
-      portalNode.remove();
-    };
-  }, [portalNode]);
+      portalNode.remove()
+    }
+  }, [portalNode])
 
-  return ReactDom.createPortal(children, portalNode);
+  return ReactDom.createPortal(children, portalNode)
 }
--- src/pages/coin/[id].js
+++ src/pages/coin/[id].js
@@ -1,42 +1,43 @@
-import { useState, useEffect, lazy, Suspense } from "react";
-import { useRouter } from "next/router";
-import { grafic7Days, oneCoin } from "../../client/client";
-import Image from "next/image";
-import useConstansGrafic from "../../hook/useConstansGrafic";
-import { color } from "../../styles/colors";
-import Content from "../../components/coin/Content";
-import Convert from "../../components/coin/Convert";
-import ModalPortal from "../../components/portal/ModalPortal";
-import { GraficContextProvider } from "../../context/GraficContext";
-import Description from "../../components/coin/Description";
-import { EPCoinAdapter } from "../../adapters/EPCoinAdapter";
+import { useState, useEffect, lazy, Suspense } from 'react'
+import { useRouter } from 'next/router'
+import { grafic7Days, oneCoin } from '../../client/client'
+import Image from 'next/image'
+import useConstansGrafic from '../../hook/useConstansGrafic'
+import { color } from '../../styles/colors'
+import Content from '../../components/coin/Content'
+import Convert from '../../components/coin/Convert'
+import ModalPortal from '../../components/portal/ModalPortal'
+import { GraficContextProvider } from '../../context/GraficContext'
+import Description from '../../components/coin/Description'
+import { EPCoinAdapter } from '../../adapters/EPCoinAdapter'
 
 const ContainerGrafic = lazy(() =>
-  import("../../components/coin/ContainerGrafic")
-);
+  import('../../components/coin/ContainerGrafic')
+)
 
 export default function Coin() {
-  const [coin, setCoin] = useState(null);
-  const { width } = useConstansGrafic();
-  const [loading, setLoading] = useState(false);
-  const [portalState, setPortalState] = useState(false);
+  const [coin, setCoin] = useState(null)
+  const { width } = useConstansGrafic()
+  const [loading, setLoading] = useState(false)
+  const [portalState, setPortalState] = useState(false)
+  const [bool, setBool] = useState(false)
 
-  const router = useRouter();
+  const router = useRouter()
 
   useEffect(() => {
-    const id = router.query.id;
+    const id = router.query.id
 
     if (id) {
       oneCoin(id)
         .then((data) => {
-          setCoin(data);
-          setLoading(false);
+          setCoin(data)
+          setLoading(false)
         })
         .catch((error) => {
-          console.log(error);
-        });
+          console.log(error)
+        })
     }
-  }, [router.query.id]);
+  }, [router.query.id])
 
   return (
     <>
@@ -58,7 +59,7 @@ export default function Coin() {
                     <Convert data={coin.market_data} name={coin.symbol} />
                   </>
                 ) : (
-                  <ModalPortal setPortalState={setPortalState}>
+                  <ModalPortal onClose={portalState}>
                     <ContainerGrafic
                       id={router.query.id}
                       setPortalState={setPortalState}
@@ -102,5 +103,5 @@ export default function Coin() {
         `}
       </style>
     </>
-  );
+  )
 }
