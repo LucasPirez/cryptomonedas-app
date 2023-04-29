9d87abb77ba87f9881b3720ec44dd63f40d6132c
Lucas Pirez <lucaspirez42@gmail.com>
2023-04-29 12:03:05 -0300

    UpdateTest_AddDeleteFavorites

--- src/components/__test__/Favorites/AddDeleteFavorites.test.js
+++ src/components/__test__/Favorites/AddDeleteFavorites.test.js
@@ -1,6 +1,7 @@
 import { fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import AddDeleteFavorite from '../../favorites/AddDeleteFavorite'
+import React, { useState } from 'react'
 
 describe('<AddDeleteFavorites/>', () => {
   test('test_add_favorites: add data to LocalStorgae and set Fill to true when button is clicked', () => {
@@ -8,6 +9,7 @@ describe('<AddDeleteFavorites/>', () => {
     const mockData = 'bitcoin'
     const mockYes = 'yellow'
     const mockNo = 'grey'
+    const mockSetItem = jest.spyOn(window.localStorage.__proto__, 'setItem')
 
     const { getByRole } = render(
       <AddDeleteFavorite
@@ -20,10 +22,20 @@ describe('<AddDeleteFavorites/>', () => {
 
     const button = getByRole('button')
     expect(button).toHaveClass('no_fill')
-
     fireEvent.click(button)
 
     expect(button).toHaveClass('fill')
-    expect(mockSetOpacity).toHaveBeenCalledWith(true)
+    expect(mockSetItem).toHaveBeenCalledWith(
+      'favorites_coin',
+      JSON.stringify([mockData])
+    )
+    fireEvent.click(button)
+
+    expect(mockSetItem).toHaveBeenCalledWith(
+      'favorites_coin',
+      JSON.stringify([])
+    )
+
+    expect(mockSetOpacity.mock.calls).toEqual([[true], [false]])
   })
 })
