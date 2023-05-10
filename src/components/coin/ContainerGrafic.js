bd2f06e6bdf799ba373b584402ea775b46d3b4d4
Lucas Pirez <lucaspirez42@gmail.com>
2023-05-10 19:00:00 -0300

    Create_DataGraficContainers_modified_ContainerGrafic

--- src/components/coin/ContainerGrafic.js
+++ src/components/coin/ContainerGrafic.js
@@ -1,103 +1,73 @@
-import { useState, useEffect, useRef } from 'react'
-import { graficDays, graficRange } from '../../client/client'
-import { color } from '../../styles/colors'
+import { useState } from 'react'
 import Grafic from './grafic/lineGrafic/Grafic'
 import CandleGrafic from './grafic/candleGrafic'
-import Image from 'next/image'
 import HeaderGeneralGrafic from './grafic/HeaderGeneralGrafic'
 import ButtonsSelectGrafic from './ButtonsSelectGrafic'
 import useGraficContext from '../../context/GraficContext'
 import Loading from '../Loading'
 import SelectorTime from './grafic/selectorTime'
-import { useSelector } from 'react-redux'
+import ModalPortal from '../portal/ModalPortal'
 
-export default function ContainerGrafic({ id, setPortalState, portalState }) {
-  const [dataBitcoin, setDataBitcoin] = useState(null)
+export default function ContainerGrafic({
+  id,
+  dataBitcoin,
+  change,
+  setChange
+}) {
   const [candleGrafic, setCandleGrafic] = useState(false)
-  const [change, setChange] = useState(false)
-  const { currencySelect } = useSelector((state) => state.criptoList)
-  const rangeMin = useRef({ min: null, time: null })
-  const { data, rangeGrafic, time, loading } = useGraficContext()
+  const [portalState, setPortalState] = useState(false)
 
-  function reduceBitcoin(value) {
-    const arr = value.map((u, i) => {
-      if (i < data.length) {
-        return [u[0], data[i][1] / u[1]]
-      }
-    })
-    setDataBitcoin(arr)
+  const { data, loading } = useGraficContext()
+
+  function GroupComponentns() {
+    return (
+      <>
+        <section>
+          <HeaderGeneralGrafic />
+
+          <ButtonsSelectGrafic
+            setPortalState={setPortalState}
+            portalState={portalState}
+            setCandleGrafic={setCandleGrafic}
+            candleGrafic={candleGrafic}
+            setChange={setChange}
+            name={id}
+            change={change}
+          />
+
+          {data &&
+            (!candleGrafic ? (
+              <Grafic data={data} change={change} dataBitcoin={dataBitcoin} />
+            ) : (
+              <CandleGrafic data={data} />
+            ))}
+          {loading && <Loading />}
+          {data && <SelectorTime id={id} currency={'usd'} />}
+        </section>
+
+        <style jsx>
+          {`
+            section {
+              position: relative;
+              width: auto;
+              height: auto;
+              padding: 20px;
+            }
+          `}
+        </style>
+      </>
+    )
   }
-
-  useEffect(() => {
-    ;(async () => {
-      if (data && time && change && rangeMin.current.time !== time) {
-        rangeMin.current.time = time
-
-        const response = await graficDays(
-          'bitcoin',
-          time,
-          currencySelect.currency
-        )
-        console.log(time)
-
-        reduceBitcoin(response.prices)
-      }
-    })()
-  }, [time, change])
-
-  useEffect(() => {
-    if (change && rangeGrafic.min !== rangeMin.current.min) {
-      rangeMin.current.min = rangeGrafic.min
-
-      graficRange({
-        id: 'bitcoin',
-        currency: currencySelect.currency,
-        time: rangeGrafic.min / 1000,
-        dateNow: rangeGrafic.max / 1000,
-      })
-        .then((datos) => datos.json())
-        .then((datos) => {
-          reduceBitcoin(datos.prices)
-        })
-    }
-  }, [rangeGrafic, change])
 
   return (
     <>
-      <section>
-        <HeaderGeneralGrafic />
-
-        <ButtonsSelectGrafic
-          setPortalState={setPortalState}
-          portalState={portalState}
-          setCandleGrafic={setCandleGrafic}
-          candleGrafic={candleGrafic}
-          setChange={setChange}
-          name={id}
-          change={change}
-        />
-
-        {data &&
-          (!candleGrafic ? (
-            <Grafic data={data} change={change} dataBitcoin={dataBitcoin} />
-          ) : (
-            <CandleGrafic data={data} />
-          ))}
-        {loading && <Loading />}
-
-        {data && <SelectorTime id={id} currency={'usd'} />}
-      </section>
-
-      <style jsx>
-        {`
-          section {
-            position: relative;
-            width: auto;
-            height: auto;
-            padding: 20px;
-          }
-        `}
-      </style>
+      {portalState ? (
+        <GroupComponentns />
+      ) : (
+        <ModalPortal>
+          <GroupComponentns />
+        </ModalPortal>
+      )}
     </>
   )
 }
---
+++ src/components/coin/DataGraficFetcher.js
@@ -0,0 +1,65 @@
+import { useState, useEffect, useRef } from 'react'
+import { graficDays, graficRange } from '../../client/client'
+import { useSelector } from 'react-redux'
+import useGraficContext from '../../context/GraficContext'
+import ContainerGrafic from './ContainerGrafic'
+
+export default function DataGraficFetcher({ id }) {
+  const [dataBitcoin, setDataBitcoin] = useState(null)
+  const { currencySelect } = useSelector((state) => state.criptoList)
+  const rangeMin = useRef({ min: null, time: null })
+  const [change, setChange] = useState(false)
+  const { data, rangeGrafic, time } = useGraficContext()
+
+  function reduceBitcoin(value) {
+    const arr = value.map((u, i) => {
+      if (i < data.length) {
+        return [u[0], data[i][1] / u[1]]
+      }
+    })
+    setDataBitcoin(arr)
+  }
+
+  useEffect(() => {
+    ;(async () => {
+      if (data && time && change && rangeMin.current.time !== time) {
+        rangeMin.current.time = time
+
+        const response = await graficDays(
+          'bitcoin',
+          time,
+          currencySelect.currency
+        )
+        console.log(time)
+
+        reduceBitcoin(response.prices)
+      }
+    })()
+  }, [time, change])
+
+  useEffect(() => {
+    if (change && rangeGrafic.min !== rangeMin.current.min) {
+      rangeMin.current.min = rangeGrafic.min
+
+      graficRange({
+        id: 'bitcoin',
+        currency: currencySelect.currency,
+        time: rangeGrafic.min / 1000,
+        dateNow: rangeGrafic.max / 1000
+      })
+        .then((datos) => datos.json())
+        .then((datos) => {
+          reduceBitcoin(datos.prices)
+        })
+    }
+  }, [rangeGrafic, change])
+
+  return (
+    <ContainerGrafic
+      dataBitcoin={dataBitcoin}
+      id={id}
+      change={change}
+      setChange={setChange}
+    />
+  )
+}
