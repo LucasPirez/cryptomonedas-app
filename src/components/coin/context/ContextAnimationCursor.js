3616216faa405425b103f23b1296588191d61844
Lucas Pirez <lucaspirez42@gmail.com>
2023-06-28 15:53:07 -0300

    moified context

--- src/components/coin/context/ContextAnimationCursor.js
+++ src/components/coin/context/ContextAnimationCursor.js
@@ -1,10 +1,11 @@
-import { createContext, useContext, useReducer } from 'react'
+import { createContext, useContext, useReducer, useRef } from 'react'
 
 const ContextAnimationCursor = createContext(null)
 
 const initialState = {
   coordenadas: { x: 0, y: 0 },
-  animationStart: false
+  animationStart: false,
+  bitcoinScale: null
 }
 
 const reducer = (state, action) => {
@@ -13,6 +14,8 @@ const reducer = (state, action) => {
       return { ...state, coordenadas: action.payload }
     case 'SET_ANIMATIONSTATE':
       return { ...state, animationStart: action.payload }
+    case 'SET_BITCOINSCALE':
+      return { ...state, bitcoinScale: action.payload }
     default:
       throw new Error('ContextAnimationCursor reducer action.type is not exist')
   }
@@ -20,8 +23,12 @@ const reducer = (state, action) => {
 
 export default function ContextAnimationCursorProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState)
+  const refBitcoinScale = useRef()
+
   return (
-    <ContextAnimationCursor.Provider value={{ state, dispatch }}>
+    <ContextAnimationCursor.Provider
+      value={{ state, dispatch, refBitcoinScale }}
+    >
       {children}
     </ContextAnimationCursor.Provider>
   )
