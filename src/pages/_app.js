d5460377fc6b0134be0cd60aac96f270d416afe5
Lucas Pirez <lucaspirez42@gmail.com>
2023-11-07 18:53:50 -0300

    add primereact dependency

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,21 @@ import { Provider } from 'react-redux'
 import { CurrencyLightProvider } from '../context/CurrencyLightContext'
 import { store } from '../redux/store'
 import Layout from '../components/layouts/Layout'
+import { PrimeReactProvider } from 'primereact/api'
+import 'primereact/resources/themes/lara-light-teal/theme.css'
+import 'primeicons/primeicons.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <CurrencyLightProvider>
-      <Provider store={store}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Provider>
-    </CurrencyLightProvider>
+    <PrimeReactProvider>
+      <CurrencyLightProvider>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
+      </CurrencyLightProvider>
+    </PrimeReactProvider>
   )
 }
 
