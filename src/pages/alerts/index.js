a84ed0dc6f7b796381c1c139eccda454c5d5aa2c
Lucas Pirez <lucaspirez42@gmail.com>
2023-11-06 21:52:46 -0300

    create alerts pages

--- src/pages/alerts/index.js
+++ src/pages/alerts/index.js
@@ -1,19 +1,32 @@
-'use client'
-
+import Head from 'next/head'
 import SearchCrypto from '../../components/alerts/createAlert/SearchCrypto'
+import AlertsCreated from '../../components/alerts/createAlert/AlertsCreated'
+import AlertContextProvider from '../../components/alerts/contextAlerts'
+import ContextToastProvider from '../../components/alerts/contextToast'
 
 export default function Alerts() {
   return (
     <>
-      <section className='container'>
-        <SearchCrypto />
-      </section>
+      <Head>
+        <title>Favorites</title>
+        <meta
+          name='description'
+          content='Effortlessly manage your favorite cryptocurrencies, monitor their market movements, and receive personalized insights to make informed investment decisions.'
+        />
+      </Head>
+      <ContextToastProvider>
+        <AlertContextProvider>
+          <section className='container'>
+            <SearchCrypto />
+            <AlertsCreated />
+          </section>
+        </AlertContextProvider>
+      </ContextToastProvider>
       <style jsx>{`
         .container {
           display: grid;
           width: 100vw;
           justify-content: center;
-
           padding: 10px;
         }
       `}</style>
